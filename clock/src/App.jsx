1e2d5c676b7611932ff7ee5edb69ac0d5c1fe48d
rahulthakur10 <67366857+rahulthakur10@users.noreply.github.com>
2021-04-03 11:20:17 +0530

    Commit-clock-example

---
+++ clock/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = ()=> {
+
+    let time = new Date().toLocaleTimeString();
+    const [ctime,setCtime] = useState(time)
+  
+    const UpdateTime = ()=>{
+        time = new Date().toLocaleTimeString();
+        setCtime(time);
+    };
+
+    setInterval(UpdateTime,1000)
+    return(
+   <>
+     <div> 
+   <h1>{ctime}</h1>
+    
+    </div>
+</>
+);
+};
+
+export default App ;
